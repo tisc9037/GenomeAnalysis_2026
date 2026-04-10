02c0e9012a41e25f88278934842c6e9c9694aeeb
Timo Schoon <tisch@pelle1.uppmax.uu.se>
2026-04-10 15:52:09 +0200

    update code

--- Code/0_preprocessing.sh
+++ Code/0_preprocessing.sh
@@ -1,1 +1,19 @@
-test
+#!/bin/bash -l
+#SBATCH -A uppmax2026-1-61
+#SBATCH -p pelle
+#SBATCH -c 1
+#SBATCH -t 00:15:00
+#SBATCH -J run_FastQ
+
+# Load modules
+module load FastQC/0.12.1-Java-11
+
+# Your commands
+INPUT_DIR=$1
+OUTPUT_DIR=$2
+
+fastqc -t 4 -o "$OUTPUT_DIR" "$INPUT_DIR"/*.fastq.gz
+
+
+
+
