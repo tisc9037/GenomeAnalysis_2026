281a084da1447c5af8dcd507361aca9068f013d2
tisch9037 <timo.schoon.9037@student.uu.se>
2026-04-16 10:42:58 +0200

    NanoPore allignment

---
+++ Code/1_genome_assembly_NanoPore.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2026-1-61
+#SBATCH -p pelle
+#SBATCH -c 4
+#SBATCH -t 03:30:00
+#SBATCH -J NanoPore_Assembly_Canu
+
+module load canu/2.3-GCCcore-13.3.0-Java-17 SAMtools/1.22.1-GCC-13.3.0
+
+INPUT_FILE=$1
+OUTPUT_DIR=$2
+
+# Run Canu
+canu \
+  -d "$OUTPUT_DIR" \
+  genomeSize=3m \
+  useGrid=false \
+  -nanopore  "$INPUT_FILE"
